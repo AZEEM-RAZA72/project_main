f73a7a2017566b79772a1e462dc541f5a658b3f9
arsalan <cse.18bcs6622gmail.com>
2024-01-14 13:41:07 +0530

    registration

---
+++ code/database.py
@@ -0,0 +1,37 @@
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, DateTime, String
+from datetime import datetime
+
+#create model class
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key= True)
+    username = Column(String(50), nullable=False)
+    email =Column(String(64),nullable=False)
+    password =Column(String(64),nullable=False)
+    created_at =Column(DateTime, default=datetime.now)
+
+    def __str__(self):
+        return self.username
+    
+#more classes for other tables
+#utility functions
+def open_db():
+        engine = create_engine('sqlite:///project.db',echo=True)
+        session = sessionmaker(bind=engine)
+        return session()
+def add_to_db(object):
+     db = open_db()
+     db.add(object)
+     db.commit()
+     db.close()
+
+if __name__=="__main__":
+    ##create engine
+    engine = create_engine('sqlite:///project.db',echo=True)
+    Base.metadata.create_all(engine)
+
+
---
+++ documentation/README.md
@@ -0,0 +1,4 @@
+run this command to install the dependencies - libraries
+```
+pip install -r requirements.txt
+```
